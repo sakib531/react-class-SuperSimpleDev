7f9c7930f37625226ab2ca7559b732e92e899d4f
sakib531 <mdsakibahammad531@gmail.com>
2025-10-17 13:52:11 +0600

    separate header component , tracking page , link-to

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes , Route } from 'react-router'
 import {HomePage} from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 
 
@@ -10,6 +12,8 @@ function App() {
       {/* index is shortcut for path='/' */}
       <Route index element={<HomePage/>}/> 
       <Route path='checkout' element={<CheckoutPage/>}></Route>
+      <Route path='orders' element={<OrdersPage/>}/>
+      <Route path='tracking' element={<TrackingPage/>}/>
     </Routes>
   )
 }
