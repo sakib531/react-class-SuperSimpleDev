a2c8d37cb06d568e554f8770c5567b6caedb05a3
sakib531 <mdsakibahammad531@gmail.com>
2025-10-25 01:53:46 +0600

    payment html with backend

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -13,10 +13,11 @@ function App() {
   const [cart ,setCart] = useState([]);
 
   useEffect (() => {
+
     axios.get('/api/cart-items?expand=product')
     .then((response) => {
       setCart(response.data)
-    })
+    });
   },[]);
 
   return (
